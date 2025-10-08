0fed6bdeb4264d6f4073495d52114ab6bb60a133
jeovaofilho-maker <jeovaofilho@gmail.com>
2025-10-08 21:19:06 +0000

    Atualização super trunfo Jeová

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
+//Jeová Ricardo
 int main() {
     // Variáveis da Carta 1 
     char estado1[20];
